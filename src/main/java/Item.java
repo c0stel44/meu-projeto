7df03b2b63a058ef3de56c721292787b49309b6b
c0stel44 <kevenzix@gmail.com>
2025-05-31 12:47:58 -0300

    primeiro git

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -2,11 +2,15 @@ public class Item {
 	private String nome;
 	private int codigo;
 	private double preco;
+	private double desconto;
+
 	
-	public Item(String nome, double preco, int codigo) {
+	public Item(String nome, double preco, int codigo, double desconto) {
 		this.nome = nome;
 		this.preco = preco;
 		this.codigo = codigo;
+		this.desconto = desconto;
+	
 	}
 
 	public String getNome() {
@@ -32,6 +36,12 @@ public class Item {
 	public void setPreço(double preco) {
 		this.preco = preco;
 	}
+	public void getDesconto( double desconto){
+
+	}public void setDesconto( double desconto){
+
+	}
+
 	
 public void Apresentar(){
    System.out.println("nome do item "+nome+" codigo do item "+codigo+" preço do item "+preco);
